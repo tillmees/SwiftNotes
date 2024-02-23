34b51db0e6e428a021dfca724ceff75432e76b37
till meeske <till.meeske@gmx.de>
2024-02-23 20:35:54 +0100

    Update exe generation script

--- src/main.py
+++ src/main.py
@@ -8,18 +8,12 @@ from classes.MainWindow import MainWindow
 def main():
     app = QApplication(sys.argv)
 
-    # with open(os.path.join("ui_windows", "style_dark.qss")) as style_file:
-    #     style_sheet = style_file.read()
-    # app.setStyleSheet(style_sheet)
-
     main_window = MainWindow(app, version="0.1.0")
     main_window.show()
 
-    app.exec()
-
-    # sys.exit(
-    #     app.exec()
-    # )
+    sys.exit(
+        app.exec()
+    )
 
 
 if __name__ == "__main__":
--- utils/02_create_exe.bat
+++ utils/02_create_exe.bat
@@ -5,7 +5,7 @@ call conda activate swift_notes_py3_10
 cd ..
 mkdir exe
 cd exe
-call pyinstaller ../src/main.py --onefile --noconsole
+call pyinstaller ../src/main.py --name "SwiftNotes" --icon="../ui/icons/swift-notes.ico" --onefile --noconsole
 
 pause
 exit

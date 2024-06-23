0b85d75902528940c08aa288621bc9107593f6c1
till meeske <till.meeske@gmx.de>
2024-06-23 10:18:43 +0200

    clean up repo

--- ui/create_resources_rc.bat
+++ ui/create_resources_rc.bat
@@ -3,7 +3,6 @@
 call conda activate swift_notes_py3_10
 
 call pyside6-rcc ../ui/resources.qrc -o 		../src/resources_rc.py
-rem call pyside6-uic ../ui/main.ui -o 				../src/ui_windows/main_ui.py
 
 pause
 exit

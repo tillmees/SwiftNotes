dbf22114b6add15c6adf012b86d9bf2049a6f767
till meeske <till.meeske@gmx.de>
2024-09-13 20:01:29 +0200

    Structure utils .bat files

--- utils/create_conda_env.bat
+++ utils/create_conda_env.bat
@@ -1,6 +1,14 @@
 @echo OFF
 
-call conda env create --file environment.yml
+echo create_conda_env.bat
+echo ============================
+echo.
+
+echo create conda environment
+set condaCreateCall=conda env create --file environment.yml
+echo calling: %condaCreateCall%
+echo.
+call %condaCreateCall%
 
 pause
 exit
--- utils/create_exe.bat
+++ utils/create_exe.bat
@@ -1,11 +1,24 @@
 @echo OFF
 
-call conda activate swift_notes_py3_10
+echo create_exe.bat
+echo ============================
+echo.
 
+echo activate conda environment
+call conda activate swift_notes_py3_10
+echo.
+
+echo create directory ./exe 
 cd ..
 mkdir exe
 cd exe
-call pyinstaller ../src/main.py --name "SwiftNotes" --icon="../ui/icons/swift-notes.ico" --onefile --noconsole
+echo.
+
+echo create SwiftNotes.exe 
+set pyinstallerCall=pyinstaller ../src/main.py --name "SwiftNotes" --icon="../ui/icons/swift-notes.ico" --onefile --noconsole
+echo calling: %pyinstallerCall%
+echo.
+call %pyinstallerCall%
 
 pause
 exit
